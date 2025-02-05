0225548cec00513d55f597a93c1667d76a1b3b55
pissartel <pierreissartel@MacBook-Pro-de-pierre-2.local>
2025-02-05 04:29:05 +0100

    change app name

--- app/src/main/java/com/pissartel/picsocialapp/BulmapApp.kt
+++ app/src/main/java/com/pissartel/picsocialapp/BulmapApp.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class PicSocialApp : Application() {
+class BulmapApp : Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
